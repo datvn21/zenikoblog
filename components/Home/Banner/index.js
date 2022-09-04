2496323249625033eea7eff50173d7965f9350c2
datvn21 <dinhtiendat0201@gmail.com>
2022-09-04 20:01:00 +0700

    fix res subbanner

--- components/Home/Banner/index.js
+++ components/Home/Banner/index.js
@@ -4,7 +4,7 @@ import TopBanner from "./TopBanner";
 
 const index = () => {
   return (
-    <div className="h-[950px] sm:h-[750px] lg:h-[600px] py-[30px] px-[10px] lg:px-0 w-full flex flex-col lg:flex-row justify-center">
+    <div className="h-[1000px] sm:h-[750px] lg:h-[600px] py-[30px] px-[10px] lg:px-0 w-full flex flex-col lg:flex-row justify-center">
       <TopBanner />
       <div className="h-[620px] lg:h-[500px] sm:h-[250px] w-full lg:w-[500px] justify-around lg:my-0 my-[5px] sm:my-[5px] lg:justify-between lg:mx-[10px] flex lg:flex-col items-center flex-col sm:flex-row">
         <SubBanner />
--- components/Layout/TabBar.js
+++ components/Layout/TabBar.js
@@ -3,7 +3,7 @@ import { HomeIcon, NewspaperIcon, UserIcon } from "@heroicons/react/24/outline";
 const TabBar = () => {
   return (
     <div className="fixed bottom-0 h-[60px] w-full flex items-center justify-center lg:none z-50">
-      <div className="flex h-[50px] justify-around items-center w-[200px] rounded-full bg-yellow-300/80  ">
+      <div className="flex h-[50px] justify-around items-center w-[200px] rounded-full bg-yellow-300  ">
         <HomeIcon className="h-[30px] w-[30px] text-[#000a5a]" />
         <NewspaperIcon className="h-[30px] w-[30px] text-[#000a5a]" />
         <UserIcon className="h-[30px] w-[30px] text-[#000a5a]" />
